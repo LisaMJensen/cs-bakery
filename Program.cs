7a6a00e1e9e84227ac1182b22949c39b7cc06533
Lisa Jensen <leese_38@yahoo.com>
2019-07-19 09:24:33 -0700

    add console menus

--- Program.cs
+++ Program.cs
@@ -2,8 +2,23 @@ using System;
 
 class ProjectName
 {
-  static void Main()
-  {
-    
-  }
+    public static void OrderGoods()
+    {
+        Console.WriteLine("Would you like to make a purchase? (Y/N)");
+    string answer = Console.ReadLine();
+    if (answer == "Y" || answer == "y")
+    {
+        Console.WriteLine("How many loaves of bread? (Please enter a number)")
+        string orderedLoaves = Console.ReadLine();
+        Console.WriteLine("How many pastries? (Please enter a number)")
+        string orderedPastries = Console.ReadLine();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to Pierre's Bakery!");
+        Console.WriteLine("Menu:");
+        Console.WriteLine("Loaf of Bread - $5   OR   Buy two, get one free");
+        Console.WriteLine("Pastry - $2   OR   Three for $5");
+    }
 }
